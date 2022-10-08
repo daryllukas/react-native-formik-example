f47e9eae476e7d2d015d9fde53bc709c774c8452
Daryl Lukas <hello@daryllukas.me>
2022-10-08 21:33:01 +0200

    feat: form design

--- App.js
+++ App.js
@@ -2,14 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
+import { RegisterForm } from './src/screens/Register';
 
 export default function App() {
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <RegisterForm />
     </ApplicationProvider>
   );
 }
